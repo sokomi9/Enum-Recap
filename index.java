382ab16a7874e446f1fa3aaa6bc48368ed77e059
abiudsokomi@gmail.com <abiudsokomi@gmail.com>
2024-03-12 21:50:54 +0300

    my commit

---
+++ index.java
@@ -0,0 +1,9 @@
+enum Day {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY;
+}
+public class index{
+   
+    public static void main(String[] args) {
+        System.out.println(Day.TUESDAY);
+    }
+}
